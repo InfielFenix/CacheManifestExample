8db344c3d173a71680b1dd87abd46520c329e550
Auer Alexander <servus@alexanderauer.at>
2018-11-18 19:17:45 +0000

    Initial Commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.offline.test.manifest.CacheManifestTest.controller.Main", {
+
+	});
+});
